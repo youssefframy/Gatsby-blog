556d62e3c9a7c68e213b69dfbe40caae948d8d68
youssefframy <youssefframy>
2023-01-27 01:45:09 +0200

    test2

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,24 +15,26 @@ const Layout = ({ children }) => {
     }
   `)
 
+  
+
   return (
     <div className="dark:bg-mainBlack dark:text-white">
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div className="py-4 px-8 md:px-16 lg:px-28">
-        <hr className="w-full pb-8"/>
+      <div className="py-4 px-4 md:px-16 lg:px-24">
+        <hr className="w-full pb-6 md:pb-8"/>
         <main className="container">
           {children}
         </main>
         <div className="flex justify-start items-center gap-4 md:gap-6">
           <a alt="github account" href="https://github.com/youssefframy" target="_blank">
-            <Icon icon="fa-brands:github-alt" w={35} height={35} className="hover:text-mainPurple cursor-pointer duration-200 transition-all ease-linear"/>
+            <Icon icon="fa-brands:github-alt" w={32} height={32} className="hover:text-mainPurple cursor-pointer duration-200 transition-all ease-linear"/>
           </a>
           <a alt="Linkedin profile" href="https://www.linkedin.com/in/youssefframy/" target="_blank">
-            <Icon icon="la:linkedin-in" w={35} height={35} className="hover:text-mainPurple cursor-pointer duration-200 transition-all ease-linear"/>
+            <Icon icon="la:linkedin-in" w={32} height={32} className="hover:text-mainPurple cursor-pointer duration-200 transition-all ease-linear"/>
           </a>
         </div>
         <hr className="w-full my-4"/>
-        <footer className="lg:text-lg font-semibold pb-12">
+        <footer className="lg:text-lg font-semibold pb-2">
           © {new Date().getFullYear()}, Created by Y. R
         </footer>
       </div>
